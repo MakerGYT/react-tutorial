ea533d23f5c0822ae760ff4406c0a6d1931c11c8
maker <maker@HUAWEI.local>
2022-10-16 22:00:26 +0800

    first commit

---
+++ src/Table.js
@@ -0,0 +1,48 @@
+import React from 'react'
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    )
+}
+const TableBody = (props) => {
+    const rows = props.characterData.map((row, index)=> {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+                <td>
+                    <button onClick={() => props.removeCharacter(index)}>Deelete</button>
+                </td>
+            </tr>
+        )
+    })
+    return (
+        <tbody>{rows}</tbody>
+    )
+}
+// class Table extends Component {
+//     render() {
+//         const {characterData} = this.props
+//         return (
+//             <table>
+//                 <TableHeader />
+//                 <TableBody characterData={characterData}/>
+//             </table>
+//         )
+//     }
+// }
+const Table = (props) => {
+    const {characterData, removeCharacter} = props
+    return (
+        <table>
+            <TableHeader />
+            <TableBody characterData={characterData} removeCharacter={removeCharacter}/>
+        </table>
+    )
+}
+export default Table
